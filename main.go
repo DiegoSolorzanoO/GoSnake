e2fb3b434b1fe0f38ceb5ddac650216633a94205
DiegoSolorzanoO <diego.slrzn917@gmail.com>
2020-06-06 15:26:38 -0500

    ErasedComments

--- main.go
+++ main.go
@@ -13,27 +13,6 @@ var gm entities.Game
 
 func init() {
 	gm = entities.NewGame()
-	// var err error
-	// imgC, _, err = ebitenutil.NewImageFromFile("assets/playerhead.png", ebiten.FilterDefault) //aqui se cargan las imagenes
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// imgUW, _, err = ebitenutil.NewImageFromFile("assets/wall.png", ebiten.FilterDefault)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// imgDW, _, err = ebitenutil.NewImageFromFile("assets/wall.png", ebiten.FilterDefault)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// imgLW, _, err = ebitenutil.NewImageFromFile("assets/sidewall.png", ebiten.FilterDefault)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// imgRW, _, err = ebitenutil.NewImageFromFile("assets/sidewall.png", ebiten.FilterDefault)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
 }
 
 // Game interface of ebiten
@@ -46,13 +25,6 @@ func (g *Game) Update(screen *ebiten.Image) error {
 		return err
 	}
 	return nil
-	// g.pressed = nil
-	// for k := ebiten.Key(0); k <= ebiten.KeyMax; k++ { //este metodo tambien checa cuando se presiona una key
-	// 	if ebiten.IsKeyPressed(k) {
-	// 		g.pressed = append(g.pressed, k)
-	// 	}
-	// }
-	// return nil
 }
 
 // Draw renders the image windows every tick
@@ -60,28 +32,6 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	if err := gm.Draw(screen); err != nil {
 		fmt.Println(err)
 	}
-	// 	op := &ebiten.DrawImageOptions{}   //puntos para poner los sprites
-	// 	opUW := &ebiten.DrawImageOptions{} //upwall
-	// 	opDW := &ebiten.DrawImageOptions{} //downwall
-	// 	opRW := &ebiten.DrawImageOptions{} //rightwall
-	// 	//op.GeoM.Translate(-float64(640)/2, -float64(640)/2)
-	// 	//op.GeoM.Rotate(float64(g.count%360) * 2 * math.Pi / 360)
-	// 	g.count++
-	// 	op.GeoM.Translate(float64(g.count), 640/2)
-	// 	opUW.GeoM.Translate(0, 0)
-	// 	opDW.GeoM.Translate(0, 620)
-	// 	opRW.GeoM.Translate(620, 0)
-	// 	screen.DrawImage(imgC, op)   //se dibuja la imagen con respecto al punto
-	// 	screen.DrawImage(imgUW, nil) // respecto a 0,0
-	// 	screen.DrawImage(imgDW, opDW)
-	// 	screen.DrawImage(imgLW, nil)
-	// 	screen.DrawImage(imgRW, opRW)
-	// 	keyStrs := []string{} //aqui se van a guardar las keys presionadas
-
-	// 	for _, p := range g.pressed {
-	// 		keyStrs = append(keyStrs, p.String())
-	// 	}
-	// 	ebitenutil.DebugPrint(screen, "Hola") //aqui se imprimen en la ventana
 }
 
 // Layout : Function which executes when it needs to reajust
